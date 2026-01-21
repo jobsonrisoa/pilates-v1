280cf72448ba4c51d941f1c59dd50d7965c56184
jobsonrisoa <jobsonrisoa@gmail.com>
2026-01-20 23:27:25 -0300

    feat(api): implement Entity base (GREEN)

---
+++ apps/api/src/shared/domain/entity.base.ts
@@ -0,0 +1,45 @@
+import { randomUUID } from 'crypto';
+
+export interface EntityProps {
+  id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export abstract class Entity<T extends EntityProps> {
+  protected readonly _id: string;
+  protected readonly _createdAt: Date;
+  protected _updatedAt: Date;
+  protected props: T;
+
+  constructor(props: T) {
+    this._id = props.id ?? randomUUID();
+    this._createdAt = props.createdAt ?? new Date();
+    this._updatedAt = props.updatedAt ?? new Date();
+    this.props = props;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  public equals(entity?: Entity<T>): boolean {
+    if (entity === null || entity === undefined) {
+      return false;
+    }
+    if (this === entity) {
+      return true;
+    }
+    return this._id === entity._id;
+  }
+}
+
+
