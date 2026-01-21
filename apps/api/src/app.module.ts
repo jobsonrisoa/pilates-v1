f63cb8cdbc0dec98e159f6891380ad115fed4e15
jobsonrisoa <jobsonrisoa@gmail.com>
2026-01-20 23:28:04 -0300

    feat(api): add Prisma schema and module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { HealthModule } from '@/modules/health/health.module';
+import { PrismaModule } from '@/shared/infrastructure/database/prisma.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { HealthModule } from '@/modules/health/health.module';
       isGlobal: true,
       expandVariables: true
     }),
+    PrismaModule,
     HealthModule
   ]
 })
