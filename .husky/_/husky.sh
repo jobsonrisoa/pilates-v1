bb3b170586ca892ae80598f4c46074466271240c
Jobson <jobsonrisoa@gmail.com>
2026-01-30 22:19:58 -0300

    chore(husky): update husky internal files

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,22 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-  readonly hook_name="$(basename "$0")"
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-  if [ "$HUSKY" = "0" ]; then
-    exit 0
-  fi
-
-  if [ -f ~/.huskyrc ]; then
-    . ~/.huskyrc
-  fi
-
-  export husky_skip_init=1
-  sh -e "$0" "$@"
-  exitCode="$?"
-
-  if [ "$exitCode" != "0" ]; then
-    echo "husky - $hook_name hook exited with code $exitCode" >&2
-  fi
-
-  exit "$exitCode"
-fi
+They WILL FAIL in v10.0.0
+"
